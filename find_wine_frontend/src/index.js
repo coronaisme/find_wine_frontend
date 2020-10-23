103d1432a3f80a17c3cac47af044458c60725501
coronaisme <coronaisme@gmail.com>
2020-10-23 14:06:29 -0700

    clean

--- find_wine_frontend/src/index.js
+++ find_wine_frontend/src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom'
-import store from './store.js';
-import 'semantic-ui-css/semantic.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store.js";
+import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
